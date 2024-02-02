3ad2f9c65d34afac07fd208c1b2533dc4b19551b
Artem Radovskyi <107635756+Kisonam@users.noreply.github.com>
2024-02-02 23:30:53 +0100

    Add files via upload

---
+++ test/test/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        IEnumerable<int> numbers = new List<int> { 4, 2 ,6 };
+        IEnumerable<int> squares = new List<int> { 4, 36 , 16 };
+
+        bool result = TestForSquares(numbers, squares);
+        Console.WriteLine(result);
+
+        Console.ReadLine();
+    }
+
+    public static bool TestForSquares(IEnumerable<int> numbers, IEnumerable<int> squares)
+    {
+        if (numbers.Count() != squares.Count())
+            return false;
+
+        numbers = numbers.OrderBy(x => x);
+        squares = squares.OrderBy(x => x);
+
+        for (int i = 0; i < numbers.Count(); i++)
+        {
+            if (squares.ElementAt(i) != numbers.ElementAt(i) * numbers.ElementAt(i))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
